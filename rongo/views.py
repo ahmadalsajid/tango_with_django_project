96670616b7eb2693c946f6dd682961ac30c8e71b
Ahmad Al-Sajid <csesajid@gmail.com>
2016-10-27 03:57:14 +0600

    chapter 4 exercises done

--- rongo/views.py
+++ rongo/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rongo says hey there partner! <br/> <a href = '/rongo/about/' > About </a>")
+    # return HttpResponse("Rongo says hey there partner! <br/> <a href = '/rongo/about/' > About </a>")
+    context_dict = {'boldmessege': 'I am bold context :D'}
+    return  render(request,'rongo/index.html',context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rongo says here is about page! <br/> <a href = '/rongo/' > Index </a>")
+    # return HttpResponse("Rongo says here is about page! <br/> <a href = '/rongo/' > Index </a>")
+    context_dict = {'name': 'Ahmad Al-Sajid'}
+    return render(request,'rongo/about.html',context=context_dict)
